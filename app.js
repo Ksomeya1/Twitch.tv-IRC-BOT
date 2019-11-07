92aefe00ed49766414fa557e1d6ab6b850231163
Kota Someya <33100724+Ksomeya1@users.noreply.github.com>
2019-11-07 13:41:04 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var options = {
 	},
 	identity: {
 		username: "TotesB0T",
-		password: "oauth:fshrwjqm6lf5iqfxxepcd0cximuv32"
+		password: "oauth:6qow48biqs5ci4h9j9gu0z6dxvtcy1"
 	},
 	channels: ["totessomedotes"]
 
@@ -24,19 +24,31 @@ var options = {
 var client = new tmi.client(options);
 client.connect();
 
-//event actions
 
+//event actions
 client.on('chat', function(channel, user, message, self) {
 	//console.log("Address: " + address + " Port: " + port);
 	// below is to type stuff in chat using action command from tmi documentation
 
 	if(message === "!commands") {
-		client.action("Totessomedotes", "Here are my commands: !twitter !github !facebook !website more soon..beep boop MrDestructoid ")
+		client.action("Totessomedotes", "Here are my commands: !fightme !twitter !github !facebook !website !creator more soon..beep boop MrDestructoid ")
 		;
+	} else if(message === "!twitter") {
+		client.action("Totessomedotes", "My Twitter is.. don't got one LUL")
+	} else if(message === "!creator") {
+		client.action("Totessomedotes", "My creator is TotesSomeDOTES, check out his github or facebook for more PogChamp")
+	} else if(message === "!github") {
+		client.action("Totessomedotes", "github.com/ksomeya1 MrDestructoid")		
+	} else if(message === "!website") {
+		client.action("Totessomedotes", "DataConquer it is still under construction MrDestructoid")
+	} else if(message === "!facebook") {
+		client.action("Totessomedotes", "My facebook is PogChamp")
+	} else if(message === "!fightme") {
+		client.action("Totessomedotes", user['display-name'] + " you tryna fight me? cmonBruh I'm just a program pls no BibleThump")
 	}	
 });	
 	client.on('connected', function(address, port) {
-		client.action("Totessomedotes", "TotesB0t reporting for duty type !commands to check out my commands beep boop MrDestructoid");
+		client.action("Totessomedotes", "[TotesB0T] reporting for duty type !commands to see what I can do.. beep boop MrDestructoid");
 	
 });
 
